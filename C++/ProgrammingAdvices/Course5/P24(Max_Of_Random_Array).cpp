21074af09630bb0ed65e34ee776d2127cdb1a3c3
HazemRagabElsayed <hazem1750@gmail.com>
2026-08-03 19:27:39 +0300

    Added ProgrammingAdvices Course5 problmes
    Added P23 and P24 problems

---
+++ C++/ProgrammingAdvices/Course5/P24(Max_Of_Random_Array).cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string>
+
+using namespace std; 
+
+int ReadPositiveNumber(string message)
+{
+    int Number = 0;
+    do {
+        cout << message << "\n";
+        cin >> Number;
+    } while (Number <= 0);
+    return Number;
+}
+
+
+int RandomNumber(int From, int To)
+{
+    return rand() % (To - From + 1) + From;
+}
+
+void FillArrayWithRandomNumbers(int arr[100], short& arrlength) {
+
+    cout << "\nEnter number of elements of array:\n";
+    cin >> arrlength;
+
+    for (int i = 0; i < arrlength; i++) {
+
+        arr[i] = RandomNumber(1, 100);
+
+    }
+}
+
+
+void PrintArray(int arr[100], int arrlength) {
+
+    for (int i = 0; i < arrlength; i++) cout << arr[i] << " ";
+
+    cout << "\n";
+
+}
+
+int MaxElementArray(int arr[100], short arrlength) {
+
+    int max = 0;
+
+    for (int i = 0; i < arrlength; i++)
+
+        if (arr[i] > max) max = arr[i];
+
+    return max;
+}
+
+int main(void) {
+
+	int arr[100], short arrlength = (short)ReadPositiveNumber("Enter number of elements of array: ");
+	FillArrayWithRandomNumbers(arr, arrlength);
+
+    cout << "\nArray elements:";
+    PrintArray(arr, arrlength);
+
+	cout << "\nMax Number is : " << MaxElementArray(arr, arrlength) << "\n";
+	return 0;
+}
